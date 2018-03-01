aab7e4a389c98c0cf811383e988a9b739ee0bfbc
srodriguez96 <stanro@stanford.edu>
2018-03-01 01:30:36 -0800

    santiago check

--- app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
@@ -1,9 +1,7 @@
 package edu.stanford.cs108.bunnyworld;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Santiago Test
-    }
-
-    public void play(View view){
-        Intent intent = new Intent(this,PlayGameOptions.class);
-        startActivity(intent);
-    }
-
-    public void edit(View view){
-        Intent intent = new Intent(this,EditGame.class);
-        startActivity(intent);
     }
 }
